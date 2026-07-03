3461f9977a0b85ddfd4dc4cbe33c500f10398860
trevisharp <leonardotrevisansilio@gmail.com>
2026-07-02 23:52:05 -0300

    Add Blindness HotReload

---
+++ FlowerKit/Core/Reload/AssemblyCompiler.cs
@@ -0,0 +1,106 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Collections.Generic;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace FlowerKit.Core.Reload;
+
+/// <summary>
+/// Compile and get a Assembly.
+/// </summary>
+public class AssemblyCompiler
+{
+    public string MainDirectory { get; set; } = Environment.CurrentDirectory;
+    public List<Assembly> ExtraReferences { get; private set; } = [];
+
+    /// <summary>
+    /// Get a new assembly of compilation from files in 'MainDirectory'
+    /// </summary>
+    public virtual Assembly? Get()
+    {
+        var newAssembly = GetNewAssembly(
+            MainDirectory,
+            ExtraReferences
+        );
+        return newAssembly;
+    }
+
+    protected virtual IEnumerable<string> FindAllCSharpFiles(
+        string directory)
+    {
+        var files = Directory.GetFiles(
+            directory, "*.cs", 
+            SearchOption.AllDirectories
+        );
+        
+        var codeFiles = 
+            from file in files
+            where !file.Contains("\\bin\\")
+            where !file.Contains("\\obj\\")
+            where !file.Contains("/bin/")
+            where !file.Contains("/obj/")
+            select file;
+        
+        foreach (var file in codeFiles.Distinct())
+            yield return file;
+    }
+
+    protected virtual IEnumerable<MetadataReference> GetReferences(
+        IEnumerable<Assembly> extraRefs)
+    {
+        var assembly = Assembly.GetEntryAssembly();
+        var assemblies = assembly!
+            .GetReferencedAssemblies()
+            .Select(Assembly.Load)
+            .Append(assembly)
+            .Append(Assembly.Load("System.Linq.Expressions"))
+            .Append(Assembly.Load("System.Private.CoreLib"))
+            .Concat(extraRefs);
+        
+        return 
+            from a in assemblies
+            select a.Location into loc
+            select MetadataReference.CreateFromFile(loc);
+    }
+
+    protected virtual Assembly? GetNewAssembly(
+        string directory,
+        IEnumerable<Assembly> extraRefs)
+    {
+        var files = FindAllCSharpFiles(directory);
+        var syntaxTrees = files
+            .Select(File.ReadAllText)
+            .Select(text => CSharpSyntaxTree.ParseText(text));
+
+        var compilationOptions = new CSharpCompilationOptions(
+            OutputKind.ConsoleApplication
+        );
+        
+        var compilation = CSharpCompilation.Create(
+            "HotReloadAppend",
+            syntaxTrees: syntaxTrees,
+            references: GetReferences(extraRefs),
+            options: compilationOptions
+        );
+
+        using var ms = new MemoryStream();
+        var result = compilation.Emit(ms);
+
+        if (result.Success)
+        {
+            ms.Seek(0, SeekOrigin.Begin);
+            return Assembly.Load(ms.ToArray());
+        }
+        
+        foreach (var diagnostic in result.Diagnostics)
+        {
+            // TODO: Show errors
+        }
+        
+        return null;
+    }
+}
---
+++ FlowerKit/Core/Reload/Reloader.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+
+namespace FlowerKit.Core.Reload;
+
+/// <summary>
+/// A Assembly reloader that get a new assembly when files changed.
+/// </summary>
+public class Reloader
+{
+    public required FileWatcher Watcher { get; set; }
+    public required AssemblyCompiler Compiler { get; set; }
+
+    /// <summary>
+    /// Actions to run when files are modifieds.
+    /// </summary>
+    public List<Action> Actions { get; private set; } = [];
+
+    /// <summary>
+    /// Get the default configuration of Reloader class.
+    /// </summary>
+    public static Reloader GetDefault()
+    {
+        return new Reloader
+        {
+            Compiler = new AssemblyCompiler(),
+            Watcher = new FileWatcher()
+            .AddCSharpFilter()
+        };
+    }
+
+    public event Action<Assembly>? OnReload;
+
+    /// <summary>
+    /// Try Reload code calling OnReload on success operation.
+    /// </summary>
+    public void TryReload()
+    {
+        if (OnReload is null)
+            return;
+        if (Watcher is null)
+            return;
+        if (Compiler is null)
+            return;
+        
+        var verify = Watcher.Verify();
+        if (!verify)
+            return;
+        
+        foreach (var action in Actions)
+        {
+            if (action is null)
+                continue;
+            
+            action();
+        }
+        
+        var newAssembly = Compiler.Get();
+        if (newAssembly is null)
+            return;
+        
+        if (OnReload is not null)
+            OnReload(newAssembly);
+    }
+
+    /// <summary>
+    /// Force Reload wihtout verification.
+    /// </summary>
+    public void Reload()
+    {
+        foreach (var action in Actions)
+        {
+            if (action is null)
+                continue;
+            
+            action();
+        }
+        
+        var newAssembly = Compiler.Get();
+        if (newAssembly is null)
+            return;
+        
+        if (OnReload is not null)
+            OnReload(newAssembly);
+    }
+}
