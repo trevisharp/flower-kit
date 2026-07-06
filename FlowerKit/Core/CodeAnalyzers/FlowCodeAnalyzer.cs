a0d536cc2973256eba0068b55c29b5db7909adbe
trevisharp <leonardotrevisansilio@gmail.com>
2026-07-05 21:47:18 -0300

    add empty classes to simulate kafka and analyze code

---
+++ FlowerKit/Core/CodeAnalyzers/FlowCodeAnalyzer.cs
@@ -0,0 +1,9 @@
+namespace FlowerKit.Core.CodeAnalyzers;
+
+/// <summary>
+/// A object to analyzer flow implementations and generate insights.
+/// </summary>
+public class FlowCodeAnalyzer
+{
+    
+}
---
+++ FlowerKit/Core/Internal/NayveKafka.cs
@@ -0,0 +1,9 @@
+namespace FlowerKit.Core.Internal;
+
+/// <summary>
+/// A Kafka simulator for local run.
+/// </summary>
+internal class NayveKafka
+{
+    
+}
