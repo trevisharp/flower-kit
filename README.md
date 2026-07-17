3d120fd21f219c9af73d4e1fce569d6522e9cefc
trevisharp <leonardotrevisansilio@gmail.com>
2026-07-17 01:09:54 -0300

    start to study aggregation and projections

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Follow the flow. Build reactive applications by composing Flows.
 
 ## Sprint 2
 
-- [ ] Permitir Hotrelaod nos flows
+- [x] Permitir Hotrelaod nos flows
 - [x] Permitir analisar flows lexicamente
 - [x] Construir grafos para ser usado pelo Executor ao montar arquitetura
 - [x] Consutuir simulador local para Kafka
@@ -13,10 +13,16 @@ Follow the flow. Build reactive applications by composing Flows.
 
 ## Sprint 3
 
-- [ ] Definir Flowers e permitir construção inteligente de fluxos
+- [x] Adicionar abstração de Log
+- [x] Adicionar independency injection
 - [ ] Definir estados e agregações por queries
-- [ ] Identificar Replay automatico
 
 ## Sprint 4
+
+- [ ] Definir Flowers e permitir construção inteligente de fluxos
+- [ ] Identificar Replay automatico
+- [ ] Tratamento de exceções e cultura de erros
+
+## Sprint 5
 
 - [ ] Conectar Kafka real
--- test/Program.cs
+++ test/Program.cs
@@ -1,61 +1,95 @@
 ﻿using FlowerKit;
 
+Log.Config
+    .AddAppName("FlowerExample")
+    .AddLevel()
+    .AddTimeStamp();
+
 Runtime.Run(args);
 
-record CollatzOdd(
-    int Start,
-    int Value
+#region Events
+
+record CreateEmployeeRequested(
+    string Name,
+    decimal Wage
 ) : Event;
 
-record CollatzEven(
-    int Start,
-    int Value
+record EmployeeCreated(
+    int ID,
+    string Name,
+    decimal Wage
 ) : Event;
 
-record CollatzEnd : Event;
+record EmployeePayRaised(
+    int ID,
+    decimal WageBonus
+) : Event;
 
-record CollatzWorkflow() : Workflow(
-    Flow.On<CollatzOdd>(ctx =>
-    {
-        Console.WriteLine($"Receive number {ctx.Data}");
-        Publish<CollatzEven>.Emit(
-            ~ctx.Data,
-            Value: 3 * ctx.Data.Value + 1
-        );
-    }),
+record InternalOrder(
+    Guid ProcessID,
+    int EmployeeID,
+    int ProductID
+) : Event;
 
-    Flow.On<CollatzEven>(ctx =>
-    {
-        Console.WriteLine($"Receive number {ctx.Data}");
-        var value = ctx.Data.Value;
-        while (value % 2 == 0)
-            value /= 2;
-        
-        if (value == 1)
-            Publish<CollatzEnd>.Emit();
-        else
-            Publish<CollatzOdd>.Emit(
-                ~ctx.Data,
-                Value: value
-            );
-    }),
+record InternalOrderReject(
+    Guid ProcessID,
+    string Reason
+) : Event;
 
-    Flow.On<CollatzEnd>(ctx =>
+record InternalOrderApprove(
+    Guid ProcessID
+) : Event;
+
+#endregion
+
+#region States
+
+record Employee(
+    int ID,
+    string Name,
+    decimal Wage
+) : State();
+
+#endregion
+
+#region Workflows
+
+record EmployeeWorkflow() : Workflow(
+    Flow.On<CreateEmployeeRequested>(ctx =>
     {
-        Console.WriteLine("Conjectura valida para um numero!");
+
     })
 );
 
-record CollatzTest() : Test(
+record InternalOrderWorkflow() : Workflow(
+    Flow.On<InternalOrder>(ctx =>
+    {
+        
+    })
+);
+
+#endregion
+
+record InternalOrderTest() : Test(
     () =>
     {
-        Assert<CollatzEven>(s => s.Last.Value == 22) // Alguma execução precisa ser Even com Value 22
-            .Then<CollatzOdd>() // Após um Even precisa ter um Odd, em algum momento
-            .Then<CollatzEnd>(s => s.Events.Count > 2); // garante que End foi emitido em algum momento
-     
-        Publish<CollatzOdd>.Emit(
-            Start: 9,
-            Value: 9
+        Assert<InternalOrderApprove>();
+
+        Publish<InternalOrder>.Emit(
+            ProcessID: Guid.NewGuid(),
+            EmployeeID: 1,
+            ProductID: 1
+        );
+    },
+
+    () =>
+    {
+        Assert<InternalOrderReject>(s => s.Last.Reason == "Price too high");
+
+        Publish<InternalOrder>.Emit(
+            ProcessID: Guid.NewGuid(),
+            EmployeeID: 1,
+            ProductID: 2
         );
     }
 );
